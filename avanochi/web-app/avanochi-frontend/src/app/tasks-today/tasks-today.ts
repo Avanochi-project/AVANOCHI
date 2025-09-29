ca11f02c907ca6c5eae51e74f63d35d1ccaee94a
Marta Fraile Jara <marta.fraile.jara@avanade.com>
2025-09-29 10:10:44 +0200

    Temporizador jornada y demas

--- avanochi/web-app/avanochi-frontend/src/app/tasks-today/tasks-today.ts
+++ avanochi/web-app/avanochi-frontend/src/app/tasks-today/tasks-today.ts
@@ -14,6 +14,22 @@ interface Task {
   imports: [CommonModule]
 })
 export class TasksToday {
+  timerPaused = signal<boolean>(false);
+
+  pausarTimer() {
+    if (this.timerInterval) {
+      clearInterval(this.timerInterval);
+      this.timerInterval = null;
+      this.timerPaused.set(true);
+    }
+  }
+
+  reanudarTimer() {
+    if (!this.timerInterval && this.jornadaInicio() && !this.jornadaFin() && this.timerPaused()) {
+      this.startTimer();
+      this.timerPaused.set(false);
+    }
+  }
   jornadaInicio = signal<string | null>(null);
   jornadaFin = signal<string | null>(null);
   timer = signal<string>('00:00:00');
